b96dae4f63e342d23429fec09b33c00fbb630d2d
Mibo0419 <bolanosm95@gmail.com>
2024-05-05 23:22:24 -0400

    popular recipes initial styling

--- src/components/PopularRecipes.js
+++ src/components/PopularRecipes.js
@@ -1,34 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "../components/PopularRecipes.css";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function PopularRecipes() {
   const [popular, setPopular] = useState([]);
 
   useEffect(() => {
     getPopularRecipes();
-  },[])
+  }, []);
 
   const getPopularRecipes = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_kEY}&number=10`);
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_kEY}&number=10`
+    );
     const data = await api.json();
     console.log(data);
 
     setPopular(data.recipes);
-  }
-
-
-
+  };
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <div id="wrapper">
+        <h3>Popular Recipes</h3>
+        <Splide options={{
+          perPage: 4, arrows: false, pagination: false, drag: 'free'
+        }}>
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide>
+            <div id="card" key={recipe.id}>
+              <p>{recipe.title}</p>
+              <img src={recipe.image} />
+            </div>
+            </SplideSlide>
+          );
+        })}
+        </Splide>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PopularRecipes
+export default PopularRecipes;
