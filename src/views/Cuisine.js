29b9b1894ce871be7dbe069e3de73d42a83499d0
Mibo0419 <bolanosm95@gmail.com>
2024-05-07 19:04:10 -0400

    cuisine pg outline

--- src/views/Cuisine.js
+++ src/views/Cuisine.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {motion} from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
 
 function Cuisine() {
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_kEY}&cuisine=${name}`);
+        const recipes = await data.json();
+
+        setCuisine(recipes.results);
+    };
+
+    useEffect(() => {
+        getCuisine(params.type);
+    }, [params.type]);
+    // console.log(params.type);
+
   return (
-    <div>Cuisine</div>
+    <div>
+        {cuisine.map((item) => {
+            return(
+                <div>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
--- src/views/Pages.js
+++ src/views/Pages.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function Pages() {
   return (
