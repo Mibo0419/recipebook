5712a1a6bb5a92099fa92b68bc3d5a315d2e7ef2
Mibo0419 <bolanosm95@gmail.com>
2024-05-09 01:43:36 -0400

    link to home pg added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import Pages from "./views/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import { GiKnifeFork } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <div>
+        <GiKnifeFork />
+        <Link to={'/'}>
+        Eats
+        </Link>
+      </div>
         <Search />
         <Category />
         <Pages />
