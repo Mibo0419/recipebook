3648f83b97c8899a4a06780e93b19ed34f67811c
Mibo0419 <bolanosm95@gmail.com>
2024-05-09 22:49:14 -0400

    category css styling added

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,30 +1,43 @@
-import React from 'react'
-import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
-import {GiNoodles, GiChopsticks} from 'react-icons/gi';
-import '../components/Category.css';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import "../components/Category.css";
+import { NavLink } from "react-router-dom";
 
 function Category() {
   return (
-    <div id='category-wrapper'>
-        <NavLink to={'/cuisine/Italian'}>
-            <FaPizzaSlice/>
-            <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
-            <FaHamburger/>
-            <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
-            <GiNoodles/>
-            <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Japanese'}>
-            <GiChopsticks/>
-            <h4>Japanese</h4>
-        </NavLink>
+    <div id="category-wrapper">
+      <div id="inner-div">
+        <h2>Quick Cuisine Picks</h2>
+        <div id="links">
+          <div className="single-link">
+            <NavLink to={"/cuisine/Italian"}>
+              <FaPizzaSlice />
+              <h4>Italian</h4>
+            </NavLink>
+          </div>
+          <div className="single-link">
+            <NavLink to={"/cuisine/American"}>
+              <FaHamburger />
+              <h4>American</h4>
+            </NavLink>
+          </div>
+          <div className="single-link">
+            <NavLink to={"/cuisine/Thai"}>
+              <GiNoodles />
+              <h4>Thai</h4>
+            </NavLink>
+          </div>
+          <div className="single-link">
+            <NavLink to={"/cuisine/Japanese"}>
+              <GiChopsticks />
+              <h4>Japanese</h4>
+            </NavLink>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
