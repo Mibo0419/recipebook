fed3ab87bc7949d3696e536aa17f71350f6b351c
Mibo0419 <bolanosm95@gmail.com>
2024-05-09 23:47:44 -0400

    cuisine pg styling

--- src/views/Cuisine.js
+++ src/views/Cuisine.js
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "../views/Cuisine.css";
 
 function Cuisine() {
-    const [cuisine, setCuisine] = useState([]);
-    let params = useParams();
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
 
-    const getCuisine = async (name) => {
-        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_kEY}&cuisine=${name}`);
-        const recipes = await data.json();
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_kEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
 
-        setCuisine(recipes.results);
-    };
+    setCuisine(recipes.results);
+  };
 
-    useEffect(() => {
-        getCuisine(params.type);
-    }, [params.type]);
-    // console.log(params.type);
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+  // console.log(params.type);
 
   return (
     <div>
+      <h1>{params.type}</h1>
+      <div id="cuisine-wrapper">
         {cuisine.map((item) => {
-            return(
-                <div>
-                    <Link to={'/recipe/' + item.id}>
-                    <img src={item.image} alt={item.title} />
-                    <h4>{item.title}</h4>
-                    </Link>
-                </div>
-            )
+          return (
+            <div className="single-cuisine">
+              <Link to={"/recipe/" + item.id}>
+                <img src={item.image} alt={item.title} />
+                <h4>{item.title}</h4>
+              </Link>
+            </div>
+          );
         })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cuisine
+export default Cuisine;
